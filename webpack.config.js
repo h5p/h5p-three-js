37e01aa79af012099c6d8451304af29f0bc52904
devland <adevland@gmail.com>
2023-09-07 09:22:41 +0200

    JI-3997 change webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path =  require('path');
 module.exports = {
   entry: './three.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'dist.js'
+    path: path.resolve(__dirname, './'),
+    filename: 'three.min.js'
   },
   mode: process.env.NODE_ENV || 'production',
 }
