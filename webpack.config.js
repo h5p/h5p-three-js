5a3cdebbfadd824cf7927b1deda317400c80df07
devland <adevland@gmail.com>
2023-09-07 09:36:12 +0200

    JI-3997 update webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path =  require('path');
 module.exports = {
   entry: './three.js',
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'three.min.js'
   },
   mode: process.env.NODE_ENV || 'production',
